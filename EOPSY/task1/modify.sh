c9d16e53f6406c505418208435ddabcad4623e57
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2022-03-18 13:26:33 +0100

    chore: put dispaly help in function

--- EOPSY/task1/modify.sh
+++ EOPSY/task1/modify.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-while getopts ":h" option; do
-	echo "This script modifies file names"
-	echo "-l lowerize file names"
-	echo "-u uppercase file names"
-	echo "-r recurse into directory"
-	echo "[-r] <sed pattern> specify sed pattern that will be executed on file names"
-	echo "-h show this help information"
-done
+show_help() {
+	local OPTIND
+	while getopts ":h" opt ; do
+		echo "This script modifies file names"
+		echo "-l lowerize file names"
+		echo "-u uppercase file names"
+		echo "-r recurse into directory"
+		echo "[-r] <sed pattern> specify sed pattern that will be executed on file names"
+		echo "-h show this help information"
+	done	
+}
+
+show_help "$@"
