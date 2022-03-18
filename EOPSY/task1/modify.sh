a9ed60fa32f97dca8920575a7f916075feca3f3a
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2022-03-18 13:34:44 +0100

    fix: proper use of -h flag

--- EOPSY/task1/modify.sh
+++ EOPSY/task1/modify.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Function to detect -h flag and show help for this script
 show_help() {
-	local OPTIND
+	local OPTIND 
 	while getopts ":h" opt ; do
-		echo "This script modifies file names"
-		echo "-l lowerize file names"
-		echo "-u uppercase file names"
-		echo "-r recurse into directory"
-		echo "[-r] <sed pattern> specify sed pattern that will be executed on file names"
-		echo "-h show this help information"
+		case "$opt" in 
+			h )
+			       	echo "This script modifies file names"
+			  	echo "-l lowerize file names"
+			  	echo "-u uppercase file names"
+			  	echo "-r recurse into directory"
+			  	echo "[-r] <sed pattern> specify sed pattern that will be executed on file names"
+			  	echo "-h show this help information"
+				;;
+	  esac
 	done	
 }
 
