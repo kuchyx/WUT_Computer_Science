362ae8c36f596e4da1da89d2fdc0898b93719f5b
kuchy <krzysztofrudnicki0@gmail.com>
2022-04-06 01:21:26 +0200

    feat: display saved blog entries

--- EGUI/build-lab1-Desktop_Qt_6_2_4_GCC_64bit-Debug/moc_blogsview.cpp
+++ EGUI/build-lab1-Desktop_Qt_6_2_4_GCC_64bit-Debug/moc_blogsview.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_blogsView_t {
-    const uint offsetsAndSize[6];
-    char stringdata0[52];
+    const uint offsetsAndSize[8];
+    char stringdata0[71];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_blogsView_t, stringdata0) + ofs), len 
@@ -31,11 +31,12 @@ static const qt_meta_stringdata_blogsView_t qt_meta_stringdata_blogsView = {
     {
 QT_MOC_LITERAL(0, 9), // "blogsView"
 QT_MOC_LITERAL(10, 40), // "on_actionCreate_new_Blog_entr..."
-QT_MOC_LITERAL(51, 0) // ""
+QT_MOC_LITERAL(51, 0), // ""
+QT_MOC_LITERAL(52, 18) // "displayBlogEntries"
 
     },
     "blogsView\0on_actionCreate_new_Blog_entry_triggered\0"
-    ""
+    "\0displayBlogEntries"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,7 +46,7 @@ static const uint qt_meta_data_blogsView[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -53,9 +54,11 @@ static const uint qt_meta_data_blogsView[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   20,    2, 0x08,    1 /* Private */,
+       1,    0,   26,    2, 0x08,    1 /* Private */,
+       3,    0,   27,    2, 0x08,    2 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -68,6 +71,7 @@ void blogsView::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         (void)_t;
         switch (_id) {
         case 0: _t->on_actionCreate_new_Blog_entry_triggered(); break;
+        case 1: _t->displayBlogEntries(); break;
         default: ;
         }
     }
@@ -82,7 +86,7 @@ const QMetaObject blogsView::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_blogsView_t
 , QtPrivate::TypeAndForceComplete<blogsView, std::true_type>
-, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -109,13 +113,13 @@ int blogsView::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 2)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 1;
+        _id -= 2;
     }
     return _id;
 }
