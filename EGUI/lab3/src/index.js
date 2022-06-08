e6a3bf15c336fe70298a8a6eb0445411e18c6b44
PolishPigeon <krzysztofrudnicki0@gmail.com>
2022-06-08 03:50:18 +0200

    chore

---
+++ EGUI/lab3/src/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class MainPage extends React.Component {
+    render()
+    {
+        return
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MainPage />);
